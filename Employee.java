40083a4a6e1bb6ada6414b3eef1a74df6d101f37
srvkmr699@gmail.com <srvkmr699@gmail.com>
2019-09-27 15:13:22 +0530

    two files added

---
+++ Employee.java
@@ -0,0 +1,28 @@
+package test;
+
+public class Employee {
+	
+	private int employeeId;
+	private String employeeName;
+	private double employeeSalary;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public double getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(double employeeSalary) {
+		this.employeeSalary = employeeSalary;
+	}
+	
+}rgegregrggegre
---
+++ Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("This is main method");
+	}
+	public void display() {
+		System.out.println("this is dummy method");
+	}
+	
+}
